28cf81a1edd22c5c0f06761eda9facc6b9a3d088
macFauzi <macfauzi@gmail.com>
2021-06-15 15:46:22 +0700

    finish python fundamental

---
+++ fundamental1-konstruksi-dasar.py
@@ -0,0 +1,21 @@
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL: Eksekusi berurutan
+print('Hello World!')
+print('By Nusakoding')
+print('Tanggal 11 Juni 2021')
+print('-'* 20)
+
+# PERCABANGAN: Eksekusi terpilih
+ingin_cepat = False
+if ingin_cepat:
+    print('jalan lurus aja ya!')
+else:
+    print('Jalan lain \n')
+
+
+# PERULANGAN
+jumlah_anak = 4
+
+for index_anak in range(1, jumlah_anak + 1): # jumlah perulangan  = 5 -1 = 4
+    print(f'Halo anak # {index_anak}')
+
--- main.py
+++
@@ -1,6 +0,0 @@
-# KONSTRUKSI DASAR PYTHON
-# SEQUENTIAL
-print('Hello World!')
-print('By Nusakoding')
-print('Tanggal 11 Juni 2021')
-print('-'* 20)
